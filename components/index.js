53d0b74f60de3505f87bde1b945598ee23791ed8
Osaze Oviawe <ocubed88@gmail.com>
2022-04-02 16:20:29 +0100

    - project set-up - home scree implemented

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTcard from "./NFTcard";
+import { SubInfo, NFTTitle, EthPrice, ImageCmp, People, EndDate } from "./SubInfo";
+import { CircleButton, RectButton } from "./Button";
+
+export {
+    HomeHeader,
+    FocusedStatusBar,
+    NFTcard,
+    CircleButton,
+    RectButton,
+    SubInfo,
+    NFTTitle,
+    EthPrice,
+    ImageCmp,
+    People,
+    EndDate
+}
